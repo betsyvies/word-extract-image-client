944b2426ec817a943907abfcd761071412d7b9da
betsyvies <ing.betsyvies@gmail.com>
2019-12-12 18:03:10 -0500

    add fetch

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const imgCabify = document.getElementById('imgCabify');
-imgCabify.addEventListener('change', (event) => {
-  //console.log(URL.createObjectURL(event.target.files[0]))
-  console.log(event.target.files[0])
 
-    /*const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onloadend = function() {
-      console.log('RESULT', reader.result)
-    }
-    reader.readAsDataURL(file);*/
-})
+imgCabify.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const reader  = new FileReader();
+  reader.addEventListener("load", function () {
+    const data = {image: reader.result.slice(reader.result.indexOf(',') + 1)};
+
+    fetch('http://localhost:5000/ocr', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      mode: 'cors',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(error => console.error('Error:', error))
+  }, false);
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+});
